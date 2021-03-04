a7a7354ad1bf332c9eae7ef3afabb4da4003548b
SeonJungKim <ksj083895@gmail.com>
2021-03-04 19:37:34 +0500

    Initial Commit

---
+++ SwiftUI_Progress/SwiftUI_ProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ProgressApp.swift
+//  SwiftUI_Progress
+//
+//  Created by 김선중 on 2021/03/04.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
