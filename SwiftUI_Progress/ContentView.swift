1d8c6e441f35292a8bcce6b8dd6f293520d7655a
SeonJungKim <ksj083895@gmail.com>
2021-03-04 19:40:41 +0500

    SwiftUI 프로그래스바

--- SwiftUI_Progress/ContentView.swift
+++ SwiftUI_Progress/ContentView.swift
@@ -1,18 +1,56 @@
-//
-//  ContentView.swift
-//  SwiftUI_Progress
-//
-//  Created by 김선중 on 2021/03/04.
-//
 
 import SwiftUI
-
-struct ContentView: View {
+struct ProgressBar: View {
+    @Binding var value: Float
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle().frame(width: geometry.size.width , height: geometry.size.height)
+                    .opacity(0.3)
+                    .foregroundColor(Color(UIColor.systemTeal))
+                
+                Rectangle().frame(width: min(CGFloat(self.value)*geometry.size.width, geometry.size.width), height: geometry.size.height)
+                    .foregroundColor(Color(UIColor.systemBlue))
+                    .animation(.linear)
+            }.cornerRadius(45.0)
+        }
     }
 }
+
+struct ContentView: View {
+    @State var progressValue: Float = 0.0
+       
+    var body: some View {
+        VStack {
+                   ProgressBar(value: $progressValue).frame(height: 20)
+                   
+                   Button(action: {
+                       self.startProgressBar()
+                   }) {
+                       Text("Start Progress")
+                   }.padding()
+                   
+                   Button(action: {
+                       self.resetProgressBar()
+                   }) {
+                       Text("Reset")
+                   }
+                   
+                   Spacer()
+               }.padding()
+           }
+           
+           func startProgressBar() {
+               for _ in 0...80 {
+                   self.progressValue += 0.015
+               }
+           }
+           
+           func resetProgressBar() {
+               self.progressValue = 0.0
+           }
+       }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
